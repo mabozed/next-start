84754f18740344a8159ec2f3fb2bd915451ce24d
mabozed <mabozed.com@gmail.com>
2024-10-21 02:12:59 +0300

    Edit 3

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from 'next'
-import localFont from 'next/font/local'
+
 import './globals.css'
 
 export const metadata: Metadata = {
@@ -14,11 +14,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
+      <body className={` antialiased`}>{children}</body>
     </html>
   )
 }
